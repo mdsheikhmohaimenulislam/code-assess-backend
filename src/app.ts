fd7c2de8b7a0048331ba0d62d036a888c35ea48d
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-09-02 12:57:19 +0600

    chore: initialize backend project

---
+++ src/app.ts
@@ -0,0 +1,63 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import express, {
+	type Application,
+	type NextFunction,
+	type Request,
+	type Response,
+} from "express";
+import httpStatus from "http-status";
+import config from "./app/config/index.js";
+
+
+
+
+
+const app: Application = express();
+
+app.use(
+	cors({
+		origin: config.frontend_url,
+		credentials: true,
+	}),
+);
+
+// Enable URL-encoded form data parsing....
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to parse JSON bodies...
+app.use(express.json());
+app.use(cookieParser());
+
+
+
+app.get("/test", async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const grantIdTokenResult = await getBkashIdToken();
+
+		console.log(grantIdTokenResult);
+
+		res.status(httpStatus.OK).json({
+			success: true,
+			message: "Welcome to PH Healthcare System Backend",
+			data: null,
+		});
+	} catch (error) {
+		console.log(error);
+		next(error);
+	}
+});
+
+// Basic route...
+app.get("/", async (req: Request, res: Response) => {
+	res.status(httpStatus.OK).json({
+		success: true,
+		message: "Welcome to PH Healthcare System Backend",
+	});
+});
+
+app.use(globalErrorHandler);
+app.use(notFound);
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,34 @@
+import config from "./app/config/index.js";
+import { prisma } from "./app/lib/prisma.js";
+
+\
+
+const PORT = Number(config.port);
+
+
+const main = async () => {
+  try {
+    await prisma.$connect();
+    console.log("Connected to the database successfully.");
+
+    await redisClient.connect();
+    console.log("Redis Connected Successfully.");
+
+    await transporter.verify();
+    console.log("Nodemailer connected successfully.");
+
+    await seedSuperAdmin();
+    await seedTesterAdmin();
+    await seedTesterDoctor();
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error starting the server:", error);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
+};
+
+main();
