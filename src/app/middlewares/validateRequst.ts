8d0318e8582a8994b39b3e6be3db9847cf2a022d
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-09-02 18:17:12 +0600

    feat:Validate Requst  created done

---
+++ src/app/middlewares/validateRequst.ts
@@ -0,0 +1,27 @@
+import { catchAsync } from "../utils/catchAsync.js";
+import type { NextFunction, Request, Response } from "express";
+import { AppError } from "../utils/AppError.js";
+import z from "zod";
+import httpStatus from "http-status";
+
+export const validateRequest = (zodSchema: z.ZodObject) => {
+  return catchAsync((req: Request, res: Response, next: NextFunction) => {
+    // const payload = req.body ? req.body : {}
+    const payload = req.body ?? {};
+
+    const result = zodSchema.safeParse(payload);
+
+    if (!result.success) {
+      console.log(result.error);
+      console.log(result.error.issues);
+
+      const message = result.error.issues[0]?.message ?? "Validation failed";
+
+      throw new AppError(httpStatus.BAD_REQUEST, message);
+    }
+
+    req.body = result.data;
+
+    next();
+  });
+};
