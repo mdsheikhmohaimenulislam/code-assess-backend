ab8c091f5c8588cd0af9047bc9f75ff72344a1a9
Luffy <mdsheikhmohaimenulislam@gmail.com>
2026-09-04 20:41:11 +0600

    feat: add professional email verification template

---
+++ src/app/module/auth/auth.service.ts
@@ -0,0 +1,86 @@
+import bcrypt from "bcryptjs";
+import crypto from "crypto";
+import ejs from "ejs";
+import { AppError } from "../../utils/AppError.js";
+import type { IRegisterPayload } from "./auth.interface.js";
+import httpStatus from "http-status";
+import { prisma } from "../../lib/prisma.js";
+import { redisClient } from "../../lib/redis.js";
+import path from "path";
+import { transporter } from "../../lib/nodemailer.js";
+import config from "../../config/index.js";
+
+const register = async (payload: IRegisterPayload) => {
+  const { name, password } = payload;
+
+  const email = payload.email.trim().toLowerCase();
+
+  const isUserExists = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (isUserExists) {
+    throw new AppError(
+      httpStatus.CONFLICT,
+      "User with this email already exists",
+    );
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 8);
+
+  const expirationSeconds = 5 * 60;
+
+  const otpValue = crypto.randomInt(100000, 1000000).toString();
+
+  const otpkey = `User-registration-otp:${email}`;
+
+  await redisClient.set(otpkey, otpValue, {
+    expiration: {
+      type: "EX",
+      value: expirationSeconds,
+    },
+  });
+
+  const Registrationkey = `User-registration-data:${email}`;
+
+  const redisUserDataPayload = {
+    name,
+    email,
+    password: hashedPassword,
+  };
+
+  await redisClient.set(Registrationkey, JSON.stringify(redisUserDataPayload), {
+    expiration: {
+      type: "EX",
+      value: expirationSeconds,
+    },
+  });
+
+  const tempatePath = path.join(
+    process.cwd(),
+    "src/app/templates/registration-user-otp.ejs",
+  );
+
+  const templateData = {
+    name,
+    email,
+    otp: otpValue,
+    expirationMinutes: expirationSeconds / 60,
+  };
+
+  const html = await ejs.renderFile(tempatePath, templateData);
+
+  console.log(html);
+
+  await transporter.sendMail({
+    from: config.email_sender,
+    to: email,
+    subject: "Email Verification",
+
+    html,
+  });
+};
+
+export const AuthService = {
+  register,
+};
